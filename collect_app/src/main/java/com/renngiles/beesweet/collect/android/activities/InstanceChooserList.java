399f5df236dbddbfb23fa1cca01771151e1129a8
hogliux <fabian.renn@gmail.com>
2017-03-19 21:43:59 +0000

    Change android sdk versions and change app bundle id

---
+++ collect_app/src/main/java/com/renngiles/beesweet/collect/android/activities/InstanceChooserList.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2009 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.renngiles.beesweet.collect.android.activities;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.renngiles.beesweet.collect.android.R;
+import com.renngiles.beesweet.collect.android.adapters.ViewSentListAdapter;
+import com.renngiles.beesweet.collect.android.application.Collect;
+import com.renngiles.beesweet.collect.android.provider.InstanceProviderAPI;
+import com.renngiles.beesweet.collect.android.provider.InstanceProviderAPI.InstanceColumns;
+import com.renngiles.beesweet.collect.android.utilities.ApplicationConstants;
+
+/**
+ * Responsible for displaying all the valid instances in the instance directory.
+ *
+ * @author Yaw Anokwa (yanokwa@gmail.com)
+ * @author Carl Hartung (carlhartung@gmail.com)
+ */
+public class InstanceChooserList extends ListActivity {
+
+    private static final boolean EXIT = true;
+    private static final boolean DO_NOT_EXIT = false;
+    private AlertDialog mAlertDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // must be at the beginning of any activity that can be called from an external intent
+        try {
+            Collect.createODKDirs();
+        } catch (RuntimeException e) {
+            createErrorDialog(e.getMessage(), EXIT);
+            return;
+        }
+
+        setContentView(R.layout.chooser_list_layout);
+        TextView tv = (TextView) findViewById(R.id.status_text);
+        tv.setVisibility(View.GONE);
+        String selection;
+        String[] selectionArgs = new String[]{InstanceProviderAPI.STATUS_SUBMITTED};
+        String sortOrder = InstanceColumns.STATUS + " DESC, " + InstanceColumns.DISPLAY_NAME + " ASC";
+
+        if (getIntent().getStringExtra(ApplicationConstants.BundleKeys.FORM_MODE).equalsIgnoreCase(ApplicationConstants.FormModes.EDIT_SAVED)) {
+            setTitle(getString(R.string.review_data));
+            selection = InstanceColumns.STATUS + " != ? ";
+        } else {
+            setTitle(getString(R.string.view_sent_forms));
+            selection = InstanceColumns.STATUS + " = ? ";
+        }
+
+        Cursor c = managedQuery(InstanceColumns.CONTENT_URI, null, selection, selectionArgs, sortOrder);
+
+        String[] data = new String[]{
+                InstanceColumns.DISPLAY_NAME, InstanceColumns.DISPLAY_SUBTEXT, InstanceColumns.DELETED_DATE
+        };
+        int[] view = new int[]{
+                R.id.text1, R.id.text2, R.id.text4
+        };
+
+        // render total instance view
+        SimpleCursorAdapter instances;
+        if (getIntent().getStringExtra(ApplicationConstants.BundleKeys.FORM_MODE).equalsIgnoreCase(ApplicationConstants.FormModes.EDIT_SAVED)) {
+            instances = new SimpleCursorAdapter(this, R.layout.two_item, c, data, view);
+        } else {
+            instances = new ViewSentListAdapter(this, R.layout.two_item, c, data, view);
+        }
+
+        setListAdapter(instances);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+
+    /**
+     * Stores the path of selected instance in the parent class and finishes.
+     */
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        Cursor c = (Cursor) getListAdapter().getItem(position);
+        startManagingCursor(c);
+        Uri instanceUri =
+                ContentUris.withAppendedId(InstanceColumns.CONTENT_URI,
+                        c.getLong(c.getColumnIndex(InstanceColumns._ID)));
+
+        Collect.getInstance().getActivityLogger().logAction(this, "onListItemClick",
+                instanceUri.toString());
+
+        if (view.findViewById(R.id.visible_off).getVisibility() != View.VISIBLE) {
+            String action = getIntent().getAction();
+            if (Intent.ACTION_PICK.equals(action)) {
+                // caller is waiting on a picked form
+                setResult(RESULT_OK, new Intent().setData(instanceUri));
+            } else {
+                // the form can be edited if it is incomplete or if, when it was
+                // marked as complete, it was determined that it could be edited
+                // later.
+                String status = c.getString(c.getColumnIndex(InstanceColumns.STATUS));
+                String strCanEditWhenComplete =
+                        c.getString(c.getColumnIndex(InstanceColumns.CAN_EDIT_WHEN_COMPLETE));
+
+                boolean canEdit = status.equals(InstanceProviderAPI.STATUS_INCOMPLETE)
+                        || Boolean.parseBoolean(strCanEditWhenComplete);
+                if (!canEdit) {
+                    createErrorDialog(getString(R.string.cannot_edit_completed_form),
+                            DO_NOT_EXIT);
+                    return;
+                }
+                // caller wants to view/edit a form, so launch formentryactivity
+                Intent parentIntent = this.getIntent();
+                Intent intent = new Intent(Intent.ACTION_EDIT, instanceUri);
+                if (parentIntent.getStringExtra(ApplicationConstants.BundleKeys.FORM_MODE).equalsIgnoreCase(ApplicationConstants.FormModes.EDIT_SAVED)) {
+                    intent.putExtra(ApplicationConstants.BundleKeys.FORM_MODE, ApplicationConstants.FormModes.EDIT_SAVED);
+                } else {
+                    intent.putExtra(ApplicationConstants.BundleKeys.FORM_MODE, ApplicationConstants.FormModes.VIEW_SENT);
+                }
+                startActivity(intent);
+            }
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Collect.getInstance().getActivityLogger().logOnStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        Collect.getInstance().getActivityLogger().logOnStop(this);
+        super.onStop();
+    }
+
+    private void createErrorDialog(String errorMsg, final boolean shouldExit) {
+        Collect.getInstance().getActivityLogger().logAction(this, "createErrorDialog", "show");
+
+        mAlertDialog = new AlertDialog.Builder(this).create();
+        mAlertDialog.setIcon(android.R.drawable.ic_dialog_info);
+        mAlertDialog.setMessage(errorMsg);
+        DialogInterface.OnClickListener errorListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                switch (i) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Collect.getInstance().getActivityLogger().logAction(this,
+                                "createErrorDialog",
+                                shouldExit ? "exitApplication" : "OK");
+                        if (shouldExit) {
+                            finish();
+                        }
+                        break;
+                }
+            }
+        };
+        mAlertDialog.setCancelable(false);
+        mAlertDialog.setButton(getString(R.string.ok), errorListener);
+        mAlertDialog.show();
+    }
+
+
+}
---
+++ collect_app/src/main/java/com/renngiles/beesweet/collect/android/preferences/AggregatePreferencesActivity.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2014 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.renngiles.beesweet.collect.android.preferences;
+
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.Toast;
+
+import com.renngiles.beesweet.collect.android.R;
+import com.renngiles.beesweet.collect.android.application.Collect;
+import com.renngiles.beesweet.collect.android.utilities.AuthDialogUtility;
+import com.renngiles.beesweet.collect.android.utilities.UrlUtils;
+import com.renngiles.beesweet.collect.android.utilities.WebUtils;
+
+/**
+ * Handles aggregate specific preferences.
+ *
+ * @author Carl Hartung (chartung@nafundi.com)
+ */
+public class AggregatePreferencesActivity extends PreferenceActivity {
+
+    protected EditTextPreference mServerUrlPreference;
+    protected EditTextPreference mUsernamePreference;
+    protected EditTextPreference mPasswordPreference;
+    protected boolean mCredentialsHaveChanged = false;
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mCredentialsHaveChanged) {
+            AuthDialogUtility.setWebCredentialsFromPreferences(getBaseContext());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aggregate_preferences);
+
+        mServerUrlPreference = (EditTextPreference) findPreference(
+                PreferencesActivity.KEY_SERVER_URL);
+        mUsernamePreference = (EditTextPreference) findPreference(PreferencesActivity.KEY_USERNAME);
+        mPasswordPreference = (EditTextPreference) findPreference(PreferencesActivity.KEY_PASSWORD);
+
+        PreferenceCategory aggregatePreferences = (PreferenceCategory) findPreference(
+                getString(R.string.aggregate_preferences));
+
+        mServerUrlPreference
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference,
+                            Object newValue) {
+                        String url = newValue.toString();
+
+                        // remove all trailing "/"s
+                        while (url.endsWith("/")) {
+                            url = url.substring(0, url.length() - 1);
+                        }
+
+                        if (UrlUtils.isValidUrl(url)) {
+                            preference.setSummary(newValue.toString());
+                            return true;
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    R.string.url_error, Toast.LENGTH_SHORT)
+                                    .show();
+                            return false;
+                        }
+                    }
+                });
+        mServerUrlPreference.setSummary(mServerUrlPreference.getText());
+        mServerUrlPreference.getEditText().setFilters(
+                new InputFilter[]{new ControlCharacterFilter(), new WhitespaceFilter()});
+
+        mUsernamePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String username = newValue.toString();
+
+                // do not allow leading and trailing whitespace
+                if (!username.equals(username.trim())) {
+                    Toast.makeText(getApplicationContext(),
+                            R.string.username_error_whitespace, Toast.LENGTH_SHORT)
+                            .show();
+                    return false;
+                }
+
+                preference.setSummary(username);
+                clearCachedCrendentials();
+
+                // To ensure we update current credentials in CredentialsProvider
+                mCredentialsHaveChanged = true;
+
+                return true;
+            }
+        });
+        mUsernamePreference.setSummary(mUsernamePreference.getText());
+        mUsernamePreference.getEditText().setFilters(
+                new InputFilter[]{new ControlCharacterFilter()});
+
+        mPasswordPreference
+                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference,
+                            Object newValue) {
+                        String pw = newValue.toString();
+
+                        // do not allow leading and trailing whitespace
+                        if (!pw.equals(pw.trim())) {
+                            Toast.makeText(getApplicationContext(),
+                                    R.string.password_error_whitespace, Toast.LENGTH_SHORT)
+                                    .show();
+                            return false;
+                        }
+
+                        maskPasswordSummary(pw);
+                        clearCachedCrendentials();
+
+                        // To ensure we update current credentials in CredentialsProvider
+                        mCredentialsHaveChanged = true;
+
+                        return true;
+                    }
+                });
+
+        maskPasswordSummary(mPasswordPreference.getText());
+        mPasswordPreference.getEditText().setFilters(
+                new InputFilter[]{new ControlCharacterFilter()});
+    }
+
+    private void maskPasswordSummary(String password) {
+        mPasswordPreference.setSummary(password != null && password.length() > 0
+                ? "********"
+                : "");
+    }
+
+    private void clearCachedCrendentials() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(
+                getBaseContext());
+        String server = settings.getString(PreferencesActivity.KEY_SERVER_URL,
+                getString(R.string.default_server_url));
+        Uri u = Uri.parse(server);
+        WebUtils.clearHostCredentials(u.getHost());
+        Collect.getInstance().getCookieStore().clear();
+    }
+
+}
+
+/**
+ * Rejects edits that contain whitespace.
+ */
+class WhitespaceFilter implements InputFilter {
+    public CharSequence filter(CharSequence source, int start, int end,
+            Spanned dest, int dstart, int dend) {
+        for (int i = start; i < end; i++) {
+            if (Character.isWhitespace(source.charAt(i))) {
+                return "";
+            }
+        }
+        return null;
+    }
+}
+
+/**
+ * Rejects edits that contain control characters, including linefeed and carriage return.
+ */
+class ControlCharacterFilter implements InputFilter {
+    public CharSequence filter(CharSequence source, int start, int end,
+            Spanned dest, int dstart, int dend) {
+        for (int i = start; i < end; i++) {
+            if (Character.getType((source.charAt(i))) == Character.CONTROL) {
+                return "";
+            }
+        }
+        return null;
+    }
+}
