277ecff4e5851f3317d47e9f2eb7012d57e5e9e3
hogliux <fabian.renn@gmail.com>
2017-05-07 21:05:10 +0100

    Increased number of seconds before aborting form download

--- collect_app/src/main/java/com/renngiles/beesweet/collect/android/activities/MainMenuActivity.java
+++ collect_app/src/main/java/com/renngiles/beesweet/collect/android/activities/MainMenuActivity.java
@@ -85,7 +85,7 @@ public class MainMenuActivity extends Activity implements FormListDownloaderList
 
     private static final int PASSWORD_DIALOG = 1;
 
-    private static final int numberOfSecondsUntilFormDownloadAbort = 5;
+    private static final int numberOfSecondsUntilFormDownloadAbort = 12;
 
     // menu options
     private static final int MENU_PREFERENCES = Menu.FIRST;
